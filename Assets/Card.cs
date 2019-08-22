77d569af4a1f9eb3acb7b99f0a01f2fca6ef5a46
Rocsalt <rocsaltmm@gmail.com>
2019-08-22 15:23:39 -0500

    Added card animations
     + Flip to Faceup
     + Flip to Facedown
     + Tied animations to an animator that toggles on left click
       + Not yet distinguishing where you click so it's just globally turning cards

---
+++ Assets/Card.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    [SerializeField] GameObject card;
+    [SerializeField] bool isFaceup = false;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isFaceup = !isFaceup;
+            animator.SetBool("IsFaceup", isFaceup);
+        }
+    }
+}
