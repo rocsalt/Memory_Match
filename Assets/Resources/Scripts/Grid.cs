e79016bb4436123a2cf863b307cc7e35e42e20a1
Mike McMillan <51094768+rocsalt@users.noreply.github.com>
2019-08-26 22:31:31 -0500

    Sandbox - Created game grid and script to populate the grid with a card prefab, depopulate grid, both triggerable in game editor

---
+++ Assets/Resources/Scripts/Grid.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Grid : MonoBehaviour
+{
+    public Vector3 startingPosition;
+    public enum GridType { XbyY }
+    public GridType gridType;
+    public enum Orientation { TopLeft }
+    public Orientation orientation;
+    public int xCoords;
+    public int yCoords;
+    public int xSpacing;
+    public int ySpacing;
+    public bool createGrid = false;
+    public bool clearGrid = false;
+    public GameObject slotPrefab;
+    public List<GameObject> slots = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CreateGrid();
+        ClearGrid();
+    }
+
+    private void CreateGrid()
+    {
+        if (clearGrid)
+        {
+            clearGrid = false;
+            for (int i = 0; i < slots.Count; i++)
+            {
+                DestroyImmediate(slots[i]);
+            }
+            slots.Clear();
+        }
+    }
+
+    private void ClearGrid()
+    {
+        if (createGrid)
+        {
+            createGrid = false;
+            int index = 0;
+            for (int y = 0; y < yCoords; y++)
+            {
+                for (int x = 0; x < xCoords; x++)
+                {
+                    GameObject go = Instantiate(slotPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+                    go.name = "Slot" + index.ToString();
+
+                    switch (orientation)
+                    {
+                        case Orientation.TopLeft:
+                            go.transform.position = new Vector3(
+                                startingPosition.x + x * xSpacing,
+                                startingPosition.y,
+                                startingPosition.z - y * ySpacing
+                                );
+                            break;
+                    }
+                    go.transform.parent = gameObject.transform;
+                    index++;
+                    slots.Add(go);
+                }
+            }
+        }
+    }
+}
