6251e80c76fa358f8aa46934cc1544153981daae
Mike McMillan <51094768+rocsalt@users.noreply.github.com>
2019-08-27 23:03:34 -0500

    Upated script to pick a specific layout, load it and fill it with cards

---
+++ Assets/Resources/Scripts/Playmat.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playmat : MonoBehaviour
+{
+    [SerializeField] GameObject cardPrefab;
+    public Layout layout;
+    public GameObject board;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateLayout();
+        CreateCardsFromLayout();
+    }
+
+    void CreateLayout()
+    {
+        board = layout.GetRandomLayout();
+    }
+
+    void CreateCardsFromLayout()
+    {
+        foreach(Slot slot in layout.slots)
+        {
+            GameObject go = GameObject.Instantiate(cardPrefab, slot.transform.position, Quaternion.Euler(0, 270, 270)) as GameObject;
+            go.transform.parent = slot.transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
